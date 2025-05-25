a7adb28b21facbe021fe5d9c34f41034a036cc45
PavelMakarchuk <pavel.ma99@gmail.com>
2025-05-25 18:28:50 -0400

    style

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import './styles/policyengine.css';
 import UpratingCalculator from './components/UpratingCalculator';
+import logo from './styles/logo.svg';
 
 const App = () => {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>PolicyEngine Uprating Calculator</h1>
+        <div className="header-content">
+          <img src={logo} alt="PolicyEngine Logo" className="logo" />
+          <h1 className="title">Uprating Calculator</h1>
+        </div>
       </header>
       <UpratingCalculator />
     </div>
